2514a86e6fe0aa3e125c1e9bf793d13afa2097dc
cyndikt89 <48552481@qq.com>
2015-09-26 23:08:06 -0700

    Update cachematrix.R
    makeCacheMatrix is a function that returns a list of functions.
    Its puspose is to store a matrix and a cached value of the inverse of the matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix is a function that returns a list of functions.
 
-## Write a short comment describing this function
+## Its purpose is to store a matrix and a cached value of the inverse of the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the inverse of a "special" matrix created with makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinverse(inv)
+  inv
 }
+
+
+## Sample run:
+## > x = matrix(1:4,2,2)
+## > m = makeCacheMatrix(x)
+## > m$get()
+##      [,1] [,2]
+##[1,]    1   3
+##[2,]    2   4
+
+## No cache in the first run
+## > cacheSolve(m)
+##      [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+
+## Retrieving from the cache in the second run
+## > cacheSolve(m)
+## getting cached data
+##      [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+
